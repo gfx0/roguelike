7dbfab6ca6b498ec4b66cc341c91cb675365bfc6
Jani Makinen <coding@live.com>
2016-06-29 13:37:35 +0300

    Cleaned up main.cpp for architechture changes
    Going to write proper FSM and game engine with components for subsystems
    such as rendering etc. so this 'all in main' junk needs to get cleaned
    up.

---
+++ roguelike/globals.h
@@ -0,0 +1,3 @@
+
+//Stop whining about sprintf, if I fuck it up then I fuck it up, so be it. I'm experienced enough as a programmer that shooting myself in the foot is practically fun by now.
+#define _CRT_SECURE_NO_WARNINGS
--- roguelike/main.cpp
+++ roguelike/main.cpp
@@ -1,4 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
+#include "globals.h"
+
 #include <windows.h>
 #include <stdio.h>
 #include <SDL.h>
@@ -8,148 +9,62 @@
 
 #include "sdlhelpers.h"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-//We'll just be using square tiles for now
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+//NOTE: These square tiles won't scale with 16:9 etc.
 const int TILE_SIZE = 40;
-
-
-
-/**
-* Render the message we want to display to a texture for drawing
-* @param message The message we want to display
-* @param fontFile The font we want to use to render the text
-* @param color The color we want the text to be
-* @param fontSize The size we want the font to be
-* @param renderer The renderer to load the texture in
-* @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
-*/
-SDL_Texture* renderText(char * message, char * fontFilePath,
-	SDL_Color color, int fontSize, SDL_Renderer *renderer)
-{
-	//Open the font
-	TTF_Font *font = TTF_OpenFont(fontFilePath, fontSize);
-	if (font == nullptr) {
-		printf("TTF_OpenFont error: %s\n", SDL_GetError());
-		return nullptr;
-	}
-	//We need to first render to a surface as that's what TTF_RenderText
-	//returns, then load that surface into a texture
-	SDL_Surface *surf = TTF_RenderText_Blended(font, message, color);
-	if (surf == nullptr) {
-		TTF_CloseFont(font);
-		printf("TTF_RenderText %s\n", SDL_GetError());
-		return nullptr;
-	}
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
-	if (texture == nullptr) {
-		printf("CreateTexture %s\n", SDL_GetError());
-	}
-	//Clean up the surface and font
-	SDL_FreeSurface(surf);
-	TTF_CloseFont(font);
-	return texture;
-}
-
-
-
-
-/**
-* Loads a BMP image into a texture on the rendering device
-* @param file The BMP image file to load
-* @param ren The renderer to load the texture onto
-* @return the loaded texture, or nullptr if something went wrong.
-*/
-SDL_Texture* loadTexture(char * filePath, SDL_Renderer *ren)
-{
-	SDL_Texture *texture = IMG_LoadTexture(ren, filePath);
-	if (texture == nullptr) {
-		printf("LoadTexture error: %s\n", SDL_GetError());
-	}
-	return texture;
-}
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-* width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param w The width of the texture to draw
-* @param h The height of the texture to draw
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h) {
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	dst.w = w;
-	dst.h = h;
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-*/
-/*
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
-int w, h;
-SDL_QueryTexture(tex, NULL, NULL, &w, &h);
-renderTexture(tex, ren, x, y, w, h);
-}
-*/
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at some destination rect
-* taking a clip of the texture if desired
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param dst The destination rectangle to render the texture to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
-	SDL_Rect *clip = nullptr)
-{
-	SDL_RenderCopy(ren, tex, clip, &dst);
-}
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height and taking a clip of the texture if desired
-* If a clip is passed, the clip's width and height will be used instead of
-*	the texture's
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
-	SDL_Rect *clip = nullptr)
-{
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	if (clip != nullptr) {
-		dst.w = clip->w;
-		dst.h = clip->h;
-	}
-	else {
-		SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	}
-	renderTexture(tex, ren, dst, clip);
-}
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+
+	/****************************************************
+	* TODO:
+	* 
+	*	Store the results of these sub system 
+	*	initializations into the game engine for generic
+	*	access while progressing in the finite state
+	*	machine.
+	*	
+	*	
+	* TODO:
+	* 
+	*	Write cleanups for all textures separately.
+	*	Perhaps a TextureManager class which stores
+	*	all textures persistently in memory anyway?
+	*	
+	*	Order of cleanup / destructing objects is 
+	*	important, don't just write one cleanup function
+	*	for all types without knowing the order in which
+	*	memory allocated SDL objects should be destroyed in.
+	*
+	* TODO:
+	* 
+	*	Create a TextRenderingManager or something.
+	*	
+	*	Purpose:
+	*	
+	*		1. Static texts rendered into one texture
+	*		2. Bitmap fonts for fast changing texts?
+	*		
+	*		A combination of these two techniques should
+	*		be used for efficient text rendering if
+	*		realtime updating text fields are wanted in
+	*		this game project. Perhaps damage / hit 
+	*		indicators / floating combat text?
+	*	
+	*		See this for font atlas creation:
+	*		http://www.angelcode.com/products/bmfont/
+	*		
+	*		
+	* TODO:
+	*		Separate texture / asset management!
+	*		
+	*		Separate sub system for real.
+	*		
+	*		Cleanup is too complex due to this now.
+	*		
+	****************************************************/
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		printf("SDL_Init error: %s\n", SDL_GetError());
@@ -162,7 +77,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SDL_Window *window = SDL_CreateWindow("Roguelike", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
 		printf("CreateWindow error: %s\n", SDL_GetError());
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
 
@@ -173,7 +88,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	if (renderer == nullptr) {
 		printf("CreateRenderer error: %s\n", SDL_GetError());
 		cleanup(window);
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
 
@@ -184,7 +99,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		printf("SDL_Image IMG_Init error: %s\n", SDL_GetError());
 		cleanup(renderer);
 		cleanup(window);
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
 
@@ -193,7 +108,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	**************************/
 	if (TTF_Init() != 0) {
 		printf("TTF_Init: %s\n", SDL_GetError());
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
 
@@ -211,41 +126,45 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	//We'll render the string "TTF fonts are cool!" in white
 	//Color is in RGBA format
 	SDL_Color color = { 255, 255, 255, 255 };
-	SDL_Texture *fontImage = renderText("Level: 1 Int: 1 Str: 1 Dex: 1", fontPath, color, 32, renderer);
-	if (fontImage == nullptr) {
+	SDL_Texture *statusBarImageFontTexture = renderText("Level: 1 Int: 1 Str: 1 Dex: 1", fontPath, color, 32, renderer);
+	if (statusBarImageFontTexture == nullptr) {
 		cleanup(renderer);
 		cleanup(window);
-		IMG_Quit();
-		TTF_Quit();
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
+
 	//Get the texture w/h so we can center it in the screen
 	int iTextW, iTextH;
-	SDL_QueryTexture(fontImage, NULL, NULL, &iTextW, &iTextH);
+	SDL_QueryTexture(statusBarImageFontTexture, NULL, NULL, &iTextW, &iTextH);
 	int xText = 0;//SCREEN_WIDTH / 2 - iTextW / 2;
 	int yText = 0;// SCREEN_HEIGHT / 2 - iTextH / 2;
 
 	/**************************
 	* Load Textures
 	**************************/
-	char backgroundImagePath[1024];
-	sprintf(backgroundImagePath, "%sgfx\\grasstile.png", resourcePath);
+	char grassTileImagePath[1024];
+	sprintf(grassTileImagePath, "%sgfx\\grasstile.png", resourcePath);
+	char wallTileImagePath[1024];
+	sprintf(wallTileImagePath, "%sgfx\\walltile.png", resourcePath);
 	char foregroundImagePath[1024];
 	sprintf(foregroundImagePath, "%sgfx\\knight.png", resourcePath);
 	char spriteSheetTestPath[1024];
 	sprintf(spriteSheetTestPath, "%sgfx\\testspritesheet.png", resourcePath);
 
-	SDL_Texture *background = loadTexture(backgroundImagePath, renderer);
+	SDL_Texture *background = loadTexture(grassTileImagePath, renderer);
+	SDL_Texture *wallTileTexture = loadTexture(wallTileImagePath, renderer);
 	SDL_Texture *playerSprite = loadTexture(foregroundImagePath, renderer);
 	SDL_Texture *testSpriteSheet = loadTexture(spriteSheetTestPath, renderer);
 
-	if (background == nullptr || playerSprite == nullptr) {
+	if (background == NULL|| playerSprite == NULL || wallTileTexture == NULL || testSpriteSheet == NULL ) {
+		cleanup(testSpriteSheet);
+		cleanup(wallTileTexture);
 		cleanup(background);
 		cleanup(playerSprite);
 		cleanup(renderer);
 		cleanup(window);
-		SDL_Quit();
+		shutdownSDL();
 		return 1;
 	}
 
@@ -345,18 +264,18 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 				TILE_SIZE);
 		}
 		renderTexture(testSpriteSheet, renderer, xx, yy, &clips[useClip]);
+		renderTexture(wallTileTexture, renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
 		renderTexture(playerSprite, renderer, x, y);
-		renderTexture(fontImage, renderer, xText, yText);
+		renderTexture(statusBarImageFontTexture, renderer, xText, yText);
 		SDL_RenderPresent(renderer);
 	}
 
+	cleanup(wallTileTexture);
 	cleanup(testSpriteSheet);
 	cleanup(playerSprite);
 	cleanup(background);
 	cleanup(renderer);
 	cleanup(window);
-	TTF_Quit();
-	IMG_Quit();
-	SDL_Quit();
+	shutdownSDL();
 	return 0;
 }
