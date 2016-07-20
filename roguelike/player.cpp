667132d6b047ac42b2d64d4508bf5bf589cd777a
mushioov <eelis.autio@gmail.com>
2016-07-20 19:18:11 +0300

    Revert "removed player class"
    This reverts commit 47b1ef1a7db581157b95c4ac1901f7ecb989e531.

---
+++ roguelike/player.cpp
@@ -0,0 +1,9 @@
+#include "globals.h"
+#include "player.h"
+#include <stdio.h>
+
+Player::Player()
+{
+	locX = locY = 0;
+	sprintf(name, "Default Player Name");
+}
---
+++ roguelike/player.h
@@ -0,0 +1,14 @@
+#ifndef player_h__
+#define player_h__
+
+class Player
+{
+public:
+	Player();
+//private://TODO: Create getters and setters etc. later.
+	char name[512];
+	float locX;
+	float locY;
+};
+
+#endif // player_h__
